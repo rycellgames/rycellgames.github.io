550215bbba9b58c22873d3875d535a82825f8345
itsprobablyjackson <77308751+itsprobablyjackson@users.noreply.github.com>
2026-01-14 11:25:08 +1100

    Update ad frame to fit and work I guess?

--- src/lib/components/advertisements/index.tsx
+++ src/lib/components/advertisements/index.tsx
@@ -1,19 +1,21 @@
 export function BannerAd() {
     return (
-        <div className="w-full min-h-30 p-3 rounded-2xl bg-main-700">
+        <div className="bg-main-700 w-full min-h-10 rounded-lg p-5 max-w-[1000px]">
             <p>Advertisement</p>
+            <div className="flex flex-col items-center justify-center">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9758035810696915"
                 crossOrigin="anonymous"></script>
             
             <ins className="adsbygoogle"
-                style={{display: "block"}}
+                style={{display: "block", height: "100%", width: "100%"}}
                 data-ad-client="ca-pub-9758035810696915"
                 data-ad-slot="5423220557"
                 data-ad-format="auto"
                 data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({ });
-            </script>
+        </script>
+        </div>    
         </div>
     )
 }
